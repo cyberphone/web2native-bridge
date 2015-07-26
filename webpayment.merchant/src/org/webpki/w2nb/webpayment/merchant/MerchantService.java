ee3d5c7e8d7fdb1621a881be8b424cb7f1866924
Anders Rundgren <anders.rundgren.net@gmail.com>
2015-07-26 17:41:57 +0200

    temp bck

---
+++ webpayment.merchant/src/org/webpki/w2nb/webpayment/merchant/MerchantService.java
@@ -0,0 +1,94 @@
+/*
+ *  Copyright 2006-2015 WebPKI.org (http://webpki.org).
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.webpki.w2nb.webpayment.merchant;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.webpki.crypto.CertificateUtil;
+import org.webpki.crypto.CustomCryptoProvider;
+
+import org.webpki.crypto.KeyStoreVerifier;
+
+import org.webpki.json.JSONX509Verifier;
+
+import org.webpki.util.ArrayUtil;
+
+import org.webpki.w2nb.webpayment.common.KeyStoreEnumerator;
+import org.webpki.w2nb.webpayment.common.ServerSigner;
+
+import org.webpki.webutil.InitPropertyReader;
+
+public class MerchantService extends InitPropertyReader implements ServletContextListener {
+
+    static final String LOGGER_NAME           = "w2nb-merchant";
+
+    static Logger logger = Logger.getLogger(LOGGER_NAME);
+    
+    static final String KEYSTORE_PASSWORD     = "key_password";
+
+    static final String BANK_ROOT             = "bank_root";
+    
+    static final String MERCHANT_EECERT       = "merchant_eecert";
+    
+    static JSONX509Verifier bankRoot;
+    
+    static ServerSigner merchantKey;
+
+    InputStream getResource(String name) throws IOException {
+        return this.getClass().getResourceAsStream(getPropertyString(name));
+    }
+    
+    JSONX509Verifier getRoot(String name) throws IOException, GeneralSecurityException {
+        KeyStore keyStore = KeyStore.getInstance ("JKS");
+        keyStore.load (null, null);
+        keyStore.setCertificateEntry ("mykey",
+                                      CertificateUtil.getCertificateFromBlob (
+                                           ArrayUtil.getByteArrayFromInputStream (getResource(name))));        
+        return new JSONX509Verifier(new KeyStoreVerifier(keyStore));
+    }
+    
+    @Override
+    public void contextDestroyed(ServletContextEvent event) {
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent event) {
+        initProperties (event);
+         try {
+            CustomCryptoProvider.forcedLoad (false);
+
+            merchantKey = new ServerSigner(new KeyStoreEnumerator(getResource(MERCHANT_EECERT),
+                                                                  getPropertyString(KEYSTORE_PASSWORD)));
+            
+            bankRoot = getRoot(BANK_ROOT);
+
+            logger.info("Web2Native Bridge Merchant-server initiated");
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "********\n" + e.getMessage() + "\n********", e);
+        }
+    }
+}
