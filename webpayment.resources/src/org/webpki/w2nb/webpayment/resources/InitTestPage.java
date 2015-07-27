043001399b779048bae68336d45ba2142137156a
Anders Rundgren <anders.rundgren.net@gmail.com>
2015-07-27 17:42:06 +0200

    test page updated to reflect new definitions...

--- webpayment.resources/src/org/webpki/w2nb/webpayment/resources/InitTestPage.java
+++ webpayment.resources/src/org/webpki/w2nb/webpayment/resources/InitTestPage.java
@@ -173,7 +173,7 @@ public class InitTestPage implements BaseProperties {
               "            }\n" +
               "            var qualifier = message[\"@qualifier\"];\n" +
               "            if ((initMode && qualifier != \"" + Messages.WALLET_IS_READY.toString() + "\" ) ||\n" +
-              "                (!initMode && qualifier != \"" + Messages.PROVIDER_GENERIC_AUTH_RES.toString()
+              "                (!initMode && qualifier != \"" + Messages.PAYER_GENERIC_AUTH_REQ.toString()
               +                "\" && qualifier != \"" + Messages.PAYER_PULL_AUTH_REQ.toString() + "\")) {\n" +  
               "                setString(\"Wrong or missing \\\"@qualifier\\\"\");\n" +
               "                closeExtension();\n" +
