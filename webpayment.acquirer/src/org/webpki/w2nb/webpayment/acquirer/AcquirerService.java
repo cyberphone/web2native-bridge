30ec9c014c62f3d7c0e49565d867bb5d6a7aeb9a
Anders Rundgren <anders.rundgren.net@gmail.com>
2015-08-29 10:38:27 +0200

    Added Acquirer

---
+++ webpayment.acquirer/src/org/webpki/w2nb/webpayment/acquirer/AcquirerService.java
@@ -0,0 +1,126 @@
+/*
+ *  Copyright 2006-2015 WebPKI.org (http://webpki.org).
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.webpki.w2nb.webpayment.acquirer;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+
+import java.security.interfaces.RSAPublicKey;
+
+import java.util.Vector;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.webpki.crypto.CertificateUtil;
+import org.webpki.crypto.CustomCryptoProvider;
+import org.webpki.crypto.KeyStoreVerifier;
+
+import org.webpki.json.JSONX509Verifier;
+
+import org.webpki.util.ArrayUtil;
+
+import org.webpki.w2nb.webpayment.common.BaseProperties;
+import org.webpki.w2nb.webpayment.common.DecryptionKeyHolder;
+import org.webpki.w2nb.webpayment.common.KeyStoreEnumerator;
+import org.webpki.w2nb.webpayment.common.ServerSigner;
+
+import org.webpki.webutil.InitPropertyReader;
+
+public class AcquirerService extends InitPropertyReader implements ServletContextListener {
+
+    static Logger logger = Logger.getLogger(AcquirerService.class.getCanonicalName());
+    
+    static final String KEYSTORE_PASSWORD     = "key_password";
+
+    static final String DECRYPTION_KEY1       = "bank_decryptionkey1";
+    static final String DECRYPTION_KEY2       = "bank_decryptionkey2";
+    
+    static final String MERCHANT_ROOT         = "merchant_root";
+    static final String CLIENT_ROOT           = "bank_client_root";
+    
+    static final String BANK_EECERT           = "bank_eecert";
+    
+    static final String ERR_MEDIA             = "err_media_type";
+    
+    static Vector<DecryptionKeyHolder> decryptionKeys = new Vector<DecryptionKeyHolder>();
+    
+    static JSONX509Verifier merchantRoot;
+    
+    static JSONX509Verifier clientRoot;
+    
+    static ServerSigner bankKey;
+    
+    static String jsonMediaType = BaseProperties.JSON_CONTENT_TYPE;
+
+    InputStream getResource(String name) throws IOException {
+        return this.getClass().getResourceAsStream(getPropertyString(name));
+    }
+    
+    void addDecryptionKey(String name) throws IOException {
+        KeyStoreEnumerator keyStoreEnumerator = new KeyStoreEnumerator(getResource(name),
+                                                                       getPropertyString(KEYSTORE_PASSWORD));
+        decryptionKeys.add(new DecryptionKeyHolder(keyStoreEnumerator.getPublicKey(),
+                                                   keyStoreEnumerator.getPrivateKey(),
+                                                   keyStoreEnumerator.getPublicKey() instanceof RSAPublicKey ?
+                BaseProperties.JOSE_RSA_OAEP_256_ALG_ID : BaseProperties.JOSE_ECDH_ES_ALG_ID));
+    }
+
+    JSONX509Verifier getRoot(String name) throws IOException, GeneralSecurityException {
+        KeyStore keyStore = KeyStore.getInstance("JKS");
+        keyStore.load (null, null);
+        keyStore.setCertificateEntry ("mykey",
+                                      CertificateUtil.getCertificateFromBlob (
+                                           ArrayUtil.getByteArrayFromInputStream (getResource(name))));        
+        return new JSONX509Verifier(new KeyStoreVerifier(keyStore));
+    }
+    
+    @Override
+    public void contextDestroyed(ServletContextEvent event) {
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent event) {
+        initProperties (event);
+         try {
+            CustomCryptoProvider.forcedLoad (false);
+
+            bankKey = new ServerSigner(new KeyStoreEnumerator(getResource(BANK_EECERT),
+                                                              getPropertyString(KEYSTORE_PASSWORD)));
+            
+            merchantRoot = getRoot(MERCHANT_ROOT);
+            clientRoot = getRoot(CLIENT_ROOT);
+
+            addDecryptionKey(DECRYPTION_KEY1);
+            addDecryptionKey(DECRYPTION_KEY2);
+            
+            if (getPropertyBoolean(ERR_MEDIA)) {
+                jsonMediaType = "text/html";
+            }
+
+            logger.info("Web2Native Bridge Bank-server initiated");
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "********\n" + e.getMessage() + "\n********", e);
+        }
+    }
+}
---
+++ webpayment.acquirer/src/org/webpki/w2nb/webpayment/acquirer/PaymentCoreServlet.java
@@ -0,0 +1,130 @@
+/*
+ *  Copyright 2006-2015 WebPKI.org (http://webpki.org).
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.webpki.w2nb.webpayment.acquirer;
+
+import java.io.IOException;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.webpki.json.JSONDecoderCache;
+import org.webpki.json.JSONObjectReader;
+import org.webpki.json.JSONObjectWriter;
+import org.webpki.json.JSONOutputFormats;
+import org.webpki.json.JSONParser;
+
+import org.webpki.w2nb.webpayment.common.BaseProperties;
+import org.webpki.w2nb.webpayment.common.PayeeIndirectModeAuthorizationRequest;
+import org.webpki.w2nb.webpayment.common.GenericAuthorizationRequest;
+import org.webpki.w2nb.webpayment.common.GenericAuthorizationResponse;
+import org.webpki.w2nb.webpayment.common.Messages;
+import org.webpki.w2nb.webpayment.common.PaymentRequest;
+
+import org.webpki.webutil.ServletUtil;
+
+public class PaymentCoreServlet extends HttpServlet implements BaseProperties {
+
+    private static final long serialVersionUID = 1L;
+    
+    static Logger logger = Logger.getLogger (PaymentCoreServlet.class.getCanonicalName());
+    
+    static int referenceId = 164006;
+    
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        JSONObjectWriter authorizationResponse = null;
+        String clientIpAddress = null;
+        try {
+            String contentType = request.getContentType();
+            if (!contentType.equals(JSON_CONTENT_TYPE)) {
+                throw new IOException("Content-Type must be \"" + JSON_CONTENT_TYPE + "\" , found: " + contentType);
+            }
+            JSONObjectReader authorizationRequest = JSONParser.parse(ServletUtil.getData(request));
+            logger.info("Received:\n" + authorizationRequest);
+
+            ////////////////////////////////////////////////////////////////////////////////////////
+            // We rationalize here by using a single end-point for both direct and indirect modes //
+            ////////////////////////////////////////////////////////////////////////////////////////
+
+            // Direct and indirect modes technically end-up as a generic request
+            GenericAuthorizationRequest genericAuthorizationRequest = null;
+
+            // A minor test is though needed for dispatching the proper message decoder...
+            if (authorizationRequest.getString(JSONDecoderCache.QUALIFIER_JSON)
+                    .equals(Messages.PAYEE_INDIRECT_AUTH_REQ.toString())) {
+
+                // Read the attested and encrypted request. Validate attestation signature
+                PayeeIndirectModeAuthorizationRequest attestedEncryptedRequest =
+                        new PayeeIndirectModeAuthorizationRequest(authorizationRequest);
+
+                // Decrypt encrypted request and validate the embedded signatures
+                genericAuthorizationRequest =
+                        attestedEncryptedRequest.getDecryptedAuthorizationRequest(BankService.decryptionKeys);
+
+                // In the indirect mode the merchant is the only one who can provide the client's IP address
+                clientIpAddress = attestedEncryptedRequest.getClientIpAddress();
+            } else {
+                // In the direct mode the request is fine "as is"
+                genericAuthorizationRequest = new GenericAuthorizationRequest(authorizationRequest);
+
+                // In the direct mode the payment provider can derive the client's IP address from the request
+                clientIpAddress = request.getRemoteAddr();
+            }
+
+            // Client IP could be used for risk-based authentication, here it is only logged
+            logger.info("Client address: " + clientIpAddress);
+
+            // Verify that the outer signature (the user's) matches the bank's client root
+            genericAuthorizationRequest.getSignatureDecoder().verify(BankService.clientRoot);
+
+            // Get the embedded (counter-signed) payment request
+            PaymentRequest paymentRequest = genericAuthorizationRequest.getPaymentRequest();
+
+            // Verify that the merchant's signature belongs to a valid merchant trust network
+            paymentRequest.getSignatureDecoder().verify(BankService.merchantRoot);
+
+            ////////////////////////////////////////////////////////////////////////////
+            // We got an authentic request.  Now we need to check available funds etc.//
+            // However, since we haven't a real bank we simply accept :-)             //
+            ////////////////////////////////////////////////////////////////////////////
+
+            // Return the authorized request
+            authorizationResponse = GenericAuthorizationResponse.encode(paymentRequest,
+                                                                        genericAuthorizationRequest.getCardType(),
+                                                                        genericAuthorizationRequest.getCardNumber(),
+                                                                        "#" + (referenceId ++),
+                                                                        BankService.bankKey);
+
+            logger.info("Returned to caller:\n" + authorizationResponse);
+            
+        } catch (Exception e) {
+            authorizationResponse = Messages.createBaseMessage(Messages.PROVIDER_GENERIC_AUTH_RES);
+            authorizationResponse.setString(ERROR_JSON, e.getMessage());
+            logger.log(Level.SEVERE, e.getMessage(), e);
+        }
+
+        response.setContentType(BankService.jsonMediaType);
+        response.setHeader("Pragma", "No-Cache");
+        response.setDateHeader("EXPIRES", 0);
+        response.getOutputStream().write(authorizationResponse.serializeJSONObject(JSONOutputFormats.NORMALIZED));
+      }
+  }
