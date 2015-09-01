39b68b8a17e5ce3d56736012aa72f1d19afd7766
Anders Rundgren <anders.rundgren.net@gmail.com>
2015-09-01 21:30:04 +0200

    Acquirer begins to run!

--- webpayment.acquirer/src/org/webpki/w2nb/webpayment/acquirer/AcquirementCoreServlet.java
+++ webpayment.acquirer/src/org/webpki/w2nb/webpayment/acquirer/AcquirementCoreServlet.java
@@ -27,18 +27,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.webpki.json.JSONDecoderCache;
 import org.webpki.json.JSONObjectReader;
 import org.webpki.json.JSONObjectWriter;
 import org.webpki.json.JSONOutputFormats;
 import org.webpki.json.JSONParser;
 
 import org.webpki.w2nb.webpayment.common.BaseProperties;
-import org.webpki.w2nb.webpayment.common.PayeeIndirectModeAuthorizationRequest;
-import org.webpki.w2nb.webpayment.common.GenericAuthorizationRequest;
-import org.webpki.w2nb.webpayment.common.GenericAuthorizationResponse;
-import org.webpki.w2nb.webpayment.common.Messages;
-import org.webpki.w2nb.webpayment.common.PaymentRequest;
 
 import org.webpki.webutil.ServletUtil;
 
@@ -64,65 +58,11 @@ public class AcquirementCoreServlet extends HttpServlet implements BasePropertie
             ////////////////////////////////////////////////////////////////////////////////////////
             // We rationalize here by using a single end-point for both direct and indirect modes //
             ////////////////////////////////////////////////////////////////////////////////////////
-
-            // Direct and indirect modes technically end-up as a generic request
-            GenericAuthorizationRequest genericAuthorizationRequest = null;
-
-            // A minor test is though needed for dispatching the proper message decoder...
-            if (authorizationRequest.getString(JSONDecoderCache.QUALIFIER_JSON)
-                    .equals(Messages.PAYEE_INDIRECT_AUTH_REQ.toString())) {
-
-                // Read the attested and encrypted request. Validate attestation signature
-                PayeeIndirectModeAuthorizationRequest attestedEncryptedRequest =
-                        new PayeeIndirectModeAuthorizationRequest(authorizationRequest);
-
-                // Decrypt encrypted request and validate the embedded signatures
-                genericAuthorizationRequest =
-                        attestedEncryptedRequest.getDecryptedAuthorizationRequest(BankService.decryptionKeys);
-
-                // In the indirect mode the merchant is the only one who can provide the client's IP address
-                clientIpAddress = attestedEncryptedRequest.getClientIpAddress();
-            } else {
-                // In the direct mode the request is fine "as is"
-                genericAuthorizationRequest = new GenericAuthorizationRequest(authorizationRequest);
-
-                // In the direct mode the payment provider can derive the client's IP address from the request
-                clientIpAddress = request.getRemoteAddr();
-            }
-
-            // Client IP could be used for risk-based authentication, here it is only logged
-            logger.info("Client address: " + clientIpAddress);
-
-            // Verify that the outer signature (the user's) matches the bank's client root
-            genericAuthorizationRequest.getSignatureDecoder().verify(BankService.clientRoot);
-
-            // Get the embedded (counter-signed) payment request
-            PaymentRequest paymentRequest = genericAuthorizationRequest.getPaymentRequest();
-
-            // Verify that the merchant's signature belongs to a valid merchant trust network
-            paymentRequest.getSignatureDecoder().verify(BankService.merchantRoot);
-
-            ////////////////////////////////////////////////////////////////////////////
-            // We got an authentic request.  Now we need to check available funds etc.//
-            // However, since we haven't a real bank we simply accept :-)             //
-            ////////////////////////////////////////////////////////////////////////////
-
-            // Return the authorized request
-            authorizationResponse = GenericAuthorizationResponse.encode(paymentRequest,
-                                                                        genericAuthorizationRequest.getCardType(),
-                                                                        genericAuthorizationRequest.getCardNumber(),
-                                                                        "#" + (referenceId ++),
-                                                                        BankService.bankKey);
-
-            logger.info("Returned to caller:\n" + authorizationResponse);
-            
         } catch (Exception e) {
-            authorizationResponse = Messages.createBaseMessage(Messages.PROVIDER_GENERIC_AUTH_RES);
-            authorizationResponse.setString(ERROR_JSON, e.getMessage());
-            logger.log(Level.SEVERE, e.getMessage(), e);
+            
         }
-
-        response.setContentType(BankService.jsonMediaType);
+ 
+        response.setContentType(JSON_CONTENT_TYPE);
         response.setHeader("Pragma", "No-Cache");
         response.setDateHeader("EXPIRES", 0);
         response.getOutputStream().write(authorizationResponse.serializeJSONObject(JSONOutputFormats.NORMALIZED));
