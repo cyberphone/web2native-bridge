6a776daae9b20daa89b639eff25384ce4785dac8
Anders Rundgren <anders.rundgren.net@gmail.com>
2015-10-09 08:19:47 +0200

    First shot at NFC!

---
+++ webpayment.webnfc/windows-build/nfc/NFCWriter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Windows.Networking.Proximity;
+
+
+namespace NFCWriter
+{
+    class NFCWriter
+    {
+        static void Main(string[] args)
+        {
+            ProximityDevice device = ProximityDevice.GetDefault();
+
+            if (device != null)
+            {
+                long messageId =
+      //            device.PublishMessage("Windows.message", "yeh");
+                device.PublishUriMessage(new Uri(args[0])); //Reuse messageId to unpublish that message.
+                Thread.Sleep(Timeout.Infinite);
+            }
+        }
+    }
+}
